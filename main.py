84abac781bd1a50be9960172e29532d77943baa5
Henry Thompson <marialouise1216@gmail.com>
2024-10-27 18:45:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+name = 'Maria'
+age = '27'
+husband = 'Henry'
+print('Hey -', name, 'are you', age, '?')
+print('Are you married to', husband, '?')
+
+my_first_boolean = True
+integer_ian = 12
+float_fiona = 1.0
+string_stella = 'Hello'
+boolean_bob = False
+
+weather = "rainy"
+weather = "sunny"
+print(weather)
+
+voice = "Hello?"
+echo = voice
+print (voice)
+print (echo)
+
+x=3
+y=7
+answer1 = x + y
+print(answer1)
+
+mug2 = "tea"
+mug1 = "coffee"
+print(mug1)
+print(mug2)
+
+input1 = input("Pick a number between 1 and 100?")
+print(input1)
+num1 = int(input1)
+print(num1)
+input2 = input("Pick a number between 1 and 100?")
+print(input2)
+num2 = int(input2)
+print(num2)
+answer2 = num1 + num2
+print(answer2)
+print("when the two numbers you chose are added together, the answer is: ", answer2)
