3e58685696d5e4a9b62e87d97656703b70ab7ede
Henry Thompson <marialouise1216@gmail.com>
2024-10-27 20:26:35 +0000

    MyMusclePal Version 4.1

--- MyMusclePal.py
+++ MyMusclePal.py
@@ -5,7 +5,7 @@ weight = int(input1)
 protein = weight * 2
 calories = weight * 30.8
 proteinCal = protein * 4
-remainingCal = calories - proteinCal 
+remainingCal = calories - proteinCal
 print('Based on your weight of ', weight, ' you need', int(calories), 'calories per day. You should aim to eat ', protein, ' grams of protein per day (which is', int(remainingCal), 'calories)')
 
 name = input("What is your name? ")
@@ -23,3 +23,16 @@ if(user_integer % 2 == 0):
     print("That number is even!")
 else:
     print("That number is odd!")
+
+weightGoal = input("What weight goal would you like to choose? (Maintenance, Gain, Loss): ")
+
+if (weightGoal == "Maintenance"):
+    print("For weight Maintenance we recommend a calorie intake of: ", weight * 30.8)
+elif (weightGoal == "Gain"):
+    print("For weight Gain we recommend a calorie intake of: ", weight * 37.4)
+elif (weightGoal == "Loss"):
+    print("For weight Loss we recommend a calorie intake of: ", weight * 24.2)
+else:
+    print("Please provide one of the previous options. ")
+
+
